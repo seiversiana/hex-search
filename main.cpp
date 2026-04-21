76ad96006b4a620d87c7de57694e5b5b2c41d288
Nile Jocson <seiversiana@gmail.com>
2026-04-22 07:18:20 +0800

    Add read file to string fn

--- main.cpp
+++ main.cpp
@@ -1,11 +1,27 @@
 // SPDX-FileCopyrightText: Copyright (C) Nile Jocson <seiversiana@gmail.com>
 // SPDX-License-Identifier: MPL-2.0
 
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
 
 
-int main(int argc, char *argv[])
+auto read_file_to_string(std::string const &path) -> std::string
 {
-	std::cout << "Hello, world!\n";
+	auto file = std::ifstream(path);
+	auto buffer = std::ostringstream {};
+
+	buffer << file.rdbuf();
+
+	return buffer.str();
+}
+
+
+
+auto main(int argc, char *argv[]) -> int
+{
+	auto const input_path = std::string(argv[1]);
+
+	std::cout << read_file_to_string(input_path);
 }
