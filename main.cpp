b68d782ef17cf20706986c804ea7c2b805900a62
Nile Jocson <seiversiana@gmail.com>
2026-04-22 06:18:24 +0800

    Test hello world

---
+++ main.cpp
@@ -0,0 +1,11 @@
+// SPDX-FileCopyrightText: Copyright (C) Nile Jocson <seiversiana@gmail.com>
+// SPDX-License-Identifier: MPL-2.0
+
+#include <iostream>
+
+
+
+int main(int argc, char *argv[])
+{
+	std::cout << "Hello, world!\n";
+}
