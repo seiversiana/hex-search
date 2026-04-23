0e10fe8c465e3b9f49879fd361ca69797431e277
Nile Jocson <seiversiana@gmail.com>
2026-04-23 17:05:11 +0800

    Move all definitions to the bottom

--- main.cpp
+++ main.cpp
@@ -13,15 +13,13 @@
 
 namespace utils
 {
-	auto safe_toupper(char const letter) -> char
-	{
-		return static_cast<char>(std::toupper(static_cast<unsigned char>(letter)));
-	}
+	auto safe_toupper(char const letter) -> char;
+	auto letter_index(char const letter) -> std::size_t;
+}
 
-	auto letter_index(char const letter) -> std::size_t
-	{
-		return safe_toupper(letter) - 'A';
-	}
+namespace files
+{
+	auto read_file(std::string const &path) -> std::string;
 }
 
 
@@ -38,61 +36,22 @@ namespace data
 		bool is_end = false;
 		std::array<std::unique_ptr<TrieNode>, 26> children {};
 
-		auto step_inserting(char const letter) -> TrieNode *
-		{
-			auto &child = children[utils::letter_index(letter)];
-			if (child == nullptr)
-			{
-				child = std::unique_ptr<TrieNode>(new TrieNode {});
-			}
-
-			return child.get();
-		}
-
-		auto step(char const letter) const -> TrieNode const *
-		{
-			return children[utils::letter_index(letter)].get();
-		}
+		auto step_inserting(char const letter) -> TrieNode *;
+		auto step(char const letter) const -> TrieNode const *;
 	};
 
 	class Trie
 	{
-	public:
-		auto insert(std::string const &word) -> void
-		{
-			TrieNode *current = &m_root;
-			for (auto const letter: word)
-			{
-				current = current->step_inserting(letter);
-			}
-
-			current->is_end = true;
-		}
-
-		auto root() const -> TrieNode const *
-		{
-			return &m_root;
-		}
-
-	private:
 		TrieNode m_root {};
+
+	public:
+		auto insert(std::string const &word) -> void;
+		auto root() const -> TrieNode const *;
 	};
 }
 
 
 
-namespace files
-{
-	auto read_file(std::string const &path) -> std::string
-	{
-		auto file = std::ifstream(path);
-		auto buffer = std::ostringstream {};
-
-		buffer << file.rdbuf();
-
-		return buffer.str();
-	}
-}
 
 
 
@@ -101,4 +60,64 @@ auto main(int argc, char *argv[]) -> int
 	auto const input_path = std::string(argv[1]);
 
 	std::cout << files::read_file(input_path);
+}
+
+
+
+
+
+
+
+// IMPLEMENTATIONS
+
+auto utils::safe_toupper(char const letter) -> char
+{
+	return static_cast<char>(std::toupper(static_cast<unsigned char>(letter)));
+}
+
+auto utils::letter_index(char const letter) -> std::size_t
+{
+	return safe_toupper(letter) - 'A';
+}
+
+auto data::TrieNode::step_inserting(char const letter) -> TrieNode *
+{
+	auto &child = children[utils::letter_index(letter)];
+	if (child == nullptr)
+	{
+		child = std::unique_ptr<TrieNode>(new TrieNode {});
+	}
+
+	return child.get();
+}
+
+auto data::TrieNode::step(char const letter) const -> TrieNode const *
+{
+	return children[utils::letter_index(letter)].get();
+}
+
+auto data::Trie::insert(std::string const &word) -> void
+{
+	auto *current = &m_root;
+	for (auto const letter: word)
+	{
+		current = current->step_inserting(letter);
+	}
+
+	current->is_end = true;
+}
+
+auto data::Trie::root() const -> TrieNode const *
+{
+	return &m_root;
+}
+
+auto files::read_file(std::string const &path) -> std::string
+{
+	auto file = std::ifstream(path);
+	auto buffer = std::ostringstream {};
+
+	buffer << file.rdbuf();
+
+	return buffer.str();
 }
