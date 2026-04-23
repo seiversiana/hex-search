d48cf79908d1fd6c3e8f08201a76f812e57d5fd9
Nile Jocson <seiversiana@gmail.com>
2026-04-23 17:05:48 +0800

    Move read_file to utils

--- main.cpp
+++ main.cpp
@@ -15,10 +15,6 @@ namespace utils
 {
 	auto safe_toupper(char const letter) -> char;
 	auto letter_index(char const letter) -> std::size_t;
-}
-
-namespace files
-{
 	auto read_file(std::string const &path) -> std::string;
 }
 
@@ -59,7 +55,7 @@ auto main(int argc, char *argv[]) -> int
 {
 	auto const input_path = std::string(argv[1]);
 
-	std::cout << files::read_file(input_path);
+	std::cout << utils::read_file(input_path);
 }
 
 
@@ -78,6 +74,16 @@ auto utils::safe_toupper(char const letter) -> char
 auto utils::letter_index(char const letter) -> std::size_t
 {
 	return safe_toupper(letter) - 'A';
+}
+
+auto utils::read_file(std::string const &path) -> std::string
+{
+	auto file = std::ifstream(path);
+	auto buffer = std::ostringstream {};
+
+	buffer << file.rdbuf();
+
+	return buffer.str();
 }
 
 auto data::TrieNode::step_inserting(char const letter) -> TrieNode *
@@ -110,14 +116,4 @@ auto data::Trie::insert(std::string const &word) -> void
 auto data::Trie::root() const -> TrieNode const *
 {
 	return &m_root;
-}
-
-auto files::read_file(std::string const &path) -> std::string
-{
-	auto file = std::ifstream(path);
-	auto buffer = std::ostringstream {};
-
-	buffer << file.rdbuf();
-
-	return buffer.str();
 }
